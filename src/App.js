3dab14525db6b890a589453b2301c0d735d58fb3
Shubham <jais.shubham26@gmail.com>
2018-12-26 18:50:38 +0100

    recipe.js added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Apps from './recipe';
 class App extends Component {
   render() {
     return (
      <div>
-       <h1>hi</h1>
+
+         <h1>Recipe Searcher</h1>
+
+         <Apps/>
      </div>
     );
   }
--- src/recipe.js
+++ src/recipe.js
@@ -1,2 +1,36 @@
 import React,{Component} from'react';
-import  {Form,FormGroup,FormControl,Controllabel,Button} from react-bootstrap;
+import  {Form,FormGroup,FormControl,ControlLabel,Button} from 'react-bootstrap';
+//import ControlLabel from "react-bootstrap/es/ControlLabel";
+
+class Apps extends Component
+{
+    render() {
+        return (
+   <Form inline>
+
+       <FormGroup>
+
+           <ControlLabel>
+               Ingredients
+           </ControlLabel> {''}
+           <FormControl   type='text' placeholder='garlic'/>
+       </FormGroup>
+       {''}
+
+
+       <FormGroup>
+           <ControlLabel> dish
+           </ControlLabel>
+               {''}
+           <FormControl type="text" placeholder='lodo'></FormControl>
+       </FormGroup>
+       {''}
+
+       <Button color={'blue'}> Submit</Button>
+   </Form>
+
+        );
+
+    }
+}
+            export default Apps;
